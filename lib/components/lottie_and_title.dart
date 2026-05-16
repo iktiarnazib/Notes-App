aa439e35ff7804a44562da9038c85e5aa958f600
Iktiar Nazib <iktiarnazib@gmail.com>
2026-05-16 13:29:50 +0600

    Onboarding page ui using the screen width mentained so that there is no overflow error

---
+++ lib/components/lottie_and_title.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:notesapp2/helper/helper.dart';
+
+class LottieAndTitle extends StatelessWidget {
+  final String lottieLocation;
+  final String title;
+  final String description;
+  const LottieAndTitle({
+    super.key,
+    required this.lottieLocation,
+    required this.title,
+    required this.description,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(height: 30),
+        //lottie
+        Lottie.asset(
+          lottieLocation,
+          height: MediaQuery.of(context).size.height * .65,
+          fit: BoxFit.contain,
+          repeat: false,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 12.0),
+          child: Text(
+            title,
+            style: TextStyle(
+              fontFamily: 'DMSerifText',
+              color: Theme.of(context).colorScheme.inversePrimary,
+              fontSize: 25,
+            ),
+            softWrap: true,
+            textAlign: TextAlign.center,
+          ),
+        ),
+        SizedBox(height: TSizes.spaceBtwItems),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Text(
+            description,
+            style: TextStyle(
+              fontFamily: 'DMSerifText',
+              color: Theme.of(context).colorScheme.inversePrimary,
+              fontSize: 16,
+            ),
+            softWrap: true,
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/onboarding/onboarding_page.dart
+++ lib/onboarding/onboarding_page.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:lottie/lottie.dart';
+import 'package:notesapp2/components/lottie_and_title.dart';
+import 'package:notesapp2/components/my_key_button.dart';
+import 'package:notesapp2/components/my_style_button.dart';
 import 'package:notesapp2/helper/helper.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 
 class OnboardingPage extends StatelessWidget {
   const OnboardingPage({super.key});
@@ -9,138 +12,78 @@ class OnboardingPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.surface,
-      appBar: AppBar(
-        title: Text('Welcome', style: TextStyle(fontFamily: 'DMSerifText')),
-        centerTitle: true,
-      ),
+
       body: Stack(
         children: [
           //horizontal scrollable pages
           PageView(
             children: [
-              Column(
-                children: [
-                  //lottie
-                  Lottie.asset(
-                    Helper.noteLottie,
-                    height: MediaQuery.of(context).size.height * .50,
-                    fit: BoxFit.contain,
-                    repeat: false,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                    child: Text(
-                      Helper.noteTitle,
-                      style: TextStyle(
-                        fontFamily: 'DMSerifText',
-                        color: Theme.of(context).colorScheme.inversePrimary,
-                        fontSize: 25,
-                      ),
-                      softWrap: true,
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                  SizedBox(height: TSizes.spaceBtwItems),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                    child: Text(
-                      Helper.noteDescription,
-                      style: TextStyle(
-                        fontFamily: 'DMSerifText',
-                        color: Theme.of(context).colorScheme.inversePrimary,
-                        fontSize: 16,
-                      ),
-                      softWrap: true,
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                ],
+              LottieAndTitle(
+                lottieLocation: Helper.noteLottie,
+                title: Helper.noteTitle,
+                description: Helper.noteDescription,
               ),
-              Column(
-                children: [
-                  Lottie.asset(
-                    Helper.lockLottie,
-                    height: MediaQuery.of(context).size.height * .50,
-                    fit: BoxFit.contain,
-                    repeat: false,
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                    child: Text(
-                      Helper.lockTitle,
-                      style: TextStyle(
-                        fontFamily: 'DMSerifText',
-                        color: Theme.of(context).colorScheme.inversePrimary,
-                        fontSize: 25,
-                      ),
-                      softWrap: true,
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                  SizedBox(height: TSizes.spaceBtwItems),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                    child: Text(
-                      Helper.lockDescription,
-                      style: TextStyle(
-                        fontFamily: 'DMSerifText',
-                        color: Theme.of(context).colorScheme.inversePrimary,
-                        fontSize: 16,
-                      ),
-                      softWrap: true,
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                ],
+              LottieAndTitle(
+                lottieLocation: Helper.lockLottie,
+                title: Helper.lockTitle,
+                description: Helper.lockDescription,
               ),
-              Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Lottie.asset(
-                      Helper.celebrateLottie,
-                      height: MediaQuery.of(context).size.height * 0.42,
-                      fit: BoxFit.contain,
-                      repeat: false,
-                    ),
-                  ),
-                  SizedBox(height: 52),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                    child: Text(
-                      Helper.celebrateTitle,
-                      style: TextStyle(
-                        fontFamily: 'DMSerifText',
-                        color: Theme.of(context).colorScheme.inversePrimary,
-                        fontSize: 25,
-                      ),
-                      softWrap: true,
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                  SizedBox(height: TSizes.spaceBtwItems),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                    child: Text(
-                      Helper.celebrateDescription,
-                      style: TextStyle(
-                        fontFamily: 'DMSerifText',
-                        color: Theme.of(context).colorScheme.inversePrimary,
-                        fontSize: 16,
-                      ),
-                      softWrap: true,
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                ],
+              LottieAndTitle(
+                lottieLocation: Helper.celebrateLottie,
+                title: Helper.celebrateTitle,
+                description: Helper.celebrateDescription,
               ),
             ],
           ),
+
           //skip button
+          Positioned(
+            top: 50,
+            right: 10,
+            child: TextButton(
+              onPressed: () {},
+              style: TextButton.styleFrom(surfaceTintColor: Colors.red),
+              child: Text(
+                'Skip',
+                style: TextStyle(
+                  fontFamily: 'DMSerifText',
+                  color: Theme.of(context).colorScheme.inversePrimary,
+                ),
+              ),
+            ),
+          ),
 
           //dot naviagtion of smoothPageIndicator
+          Positioned(
+            left: TSizes.defaultSpace,
+            bottom: 60,
+            child: SmoothPageIndicator(
+              controller: PageController(),
+              count: 3,
+              effect: ExpandingDotsEffect(
+                activeDotColor: Theme.of(context).colorScheme.inversePrimary,
+                dotColor: Theme.of(context).colorScheme.secondary,
+                dotHeight: 6,
+              ),
+            ),
+          ),
 
           //Circular button
+          Positioned(
+            right: 30,
+            bottom: 45,
+            child: FilledButton(
+              onPressed: () {},
+              style: FilledButton.styleFrom(
+                backgroundColor: Theme.of(context).colorScheme.primary,
+                foregroundColor: Theme.of(context).colorScheme.inversePrimary,
+              ),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [Text('Next'), Icon(Icons.arrow_right)],
+              ),
+            ),
+          ),
         ],
       ),
     );
